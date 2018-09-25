7134f2f63aa850d525f6eb64906539188d738cad
Александр <triplustri@mail.ru>
2018-09-25 05:23:49 +0400

    add default value for Currency.number field

--- terminal/models.py
+++ terminal/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class Сurrency(models.Model):
     denomination = models.IntegerField(unique=True)
-    number = models.IntegerField()
+    number = models.IntegerField(default=0)
